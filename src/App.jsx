f793ca208725526900b790299017d274d2de8ed3
vassilyev <vassilyev@toowto.kz>
2022-06-17 12:12:46 +0600

    Добавлен Layout для обработки вывода при роутенге

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
 import { About } from "./pages/Aboutpage";
 import { Blogpage } from "./pages/Blogpage";
 import { Notfoundpage } from "./pages/Notfoundpage";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Blog</Link>
-        <Link to="/about">About</Link>
-      </header>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/posts" element={<Blogpage></Blogpage>}></Route>
-        <Route path="*" element={<Notfoundpage></Notfoundpage>}></Route>
+        <Route path="/" element={<Layout></Layout>}>
+          <Route index element={<Homepage />}></Route>
+          <Route path="about" element={<About></About>}></Route>
+          <Route path="posts" element={<Blogpage></Blogpage>}></Route>
+          <Route path="*" element={<Notfoundpage></Notfoundpage>}></Route>
+        </Route>
       </Routes>
     </>
   );
