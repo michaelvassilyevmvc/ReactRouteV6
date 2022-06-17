aa4c0ef4eaa8ff929769e0c21a02e5298a3c813b
vassilyev <vassilyev@toowto.kz>
2022-06-17 16:34:38 +0600

    Передача параметров через роутер

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
 import { About } from "./pages/Aboutpage";
 import { Blogpage } from "./pages/Blogpage";
+import { Createpost } from "./pages/Createpost";
+import { Singlepage } from "./pages/Singlepage";
 import { Notfoundpage } from "./pages/Notfoundpage";
 import { Layout } from "./components/Layout";
+import { Editpost } from "./pages/Editpost";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route index element={<Homepage />}></Route>
           <Route path="about" element={<About></About>}></Route>
           <Route path="posts" element={<Blogpage></Blogpage>}></Route>
+          <Route path="posts/:id" element={<Singlepage></Singlepage>}></Route>
+          <Route path="posts/new" element={<Createpost></Createpost>}></Route>
+          <Route path="posts/:id/edit" element={<Editpost></Editpost>}></Route>
           <Route path="*" element={<Notfoundpage></Notfoundpage>}></Route>
         </Route>
       </Routes>
---
+++ src/pages/Singlepage.jsx
@@ -0,0 +1,26 @@
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const Singlepage = () => {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, [id]);
+
+  return (
+    <div>
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          <Link to={`/posts/${id}/edit`}>Edit this post</Link>
+        </>
+      )}
+    </div>
+  );
+};
+export { Singlepage };
