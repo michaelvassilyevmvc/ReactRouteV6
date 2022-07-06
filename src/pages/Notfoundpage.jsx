415d2da81cc829e91ab00f30e38c264337682ce1
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-07-06 14:11:02 +0600

    End of section 1. Set route in start page

--- src/pages/Notfoundpage.jsx
+++ src/pages/Notfoundpage.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom';
 const Notfoundpage = () => {
     return (
         <div>
-            This page doesn't exist. Go <a href="/">home</a>
+            This page doesn't exist. Go <Link to="/">home</Link>
         </div>
     )
 }
 
-export {Notfoundpage};
+export { Notfoundpage };
