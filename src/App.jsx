187071956331869b2a899919ac92cfa0b1b91855
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-07-06 15:26:41 +0600

    End of Section 2. General markup in Outlet component React Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 import { Homepage } from './pages/Homepage';
 import { About } from './pages/Aboutpage';
 import { Blogpage } from './pages/Blogpage';
 import { Notfoundpage } from './pages/Notfoundpage';
 
+import { Layout } from './components/Layout';
+import './index.css';
+
+
 function App() {
   return (
     <>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Blog</Link>
-        <Link to="/about">About</Link>
-      </header>
 
       <Routes>
-        <Route path='/' element={<Homepage></Homepage>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/posts' element={<Blogpage></Blogpage>}></Route>
-        <Route path='*' element={<Notfoundpage></Notfoundpage>}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/posts' element={<Blogpage />}></Route>
+          <Route path='*' element={<Notfoundpage />}></Route>
+        </Route>
       </Routes>
     </>
   );
