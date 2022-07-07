5a148e117a98442decd5bb0ad2569cc5e6e01561
vassilyev <vassilyev@toowto.kz>
2022-07-07 16:40:25 +0600

    End of Section 4. Send params from request string

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Homepage } from './pages/Homepage';
-import { About } from './pages/Aboutpage';
-import { Blogpage } from './pages/Blogpage';
-import { Notfoundpage } from './pages/Notfoundpage';
+import { Homepage } from "./pages/Homepage";
+import { About } from "./pages/Aboutpage";
+import { Blogpage } from "./pages/Blogpage";
+import { Singlepage } from "./pages/Singlepage";
+import { Notfoundpage } from "./pages/Notfoundpage";
 
-import { Layout } from './components/Layout';
-import './index.css';
-
+import { Layout } from "./components/Layout";
+import "./index.css";
+import { Createpost } from "./pages/Createpost";
+import { Editpost } from "./pages/Editpost";
 
 function App() {
   return (
     <>
-
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />}></Route>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/posts' element={<Blogpage />}></Route>
-          <Route path='*' element={<Notfoundpage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/posts" element={<Blogpage />}></Route>
+          <Route path="/posts/:id/" element={<Singlepage />}></Route>
+          <Route path="/posts/new" element={<Createpost></Createpost>}></Route>
+          <Route path="/posts/:id/edit" element={<Editpost></Editpost>}></Route>
+          <Route path="*" element={<Notfoundpage />}></Route>
         </Route>
       </Routes>
     </>
--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,9 +1,27 @@
-const Blogpage = () => {
-    return (
-        <div>
-            <h1>Our news</h1>
-        </div>
-    )
-}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export {Blogpage}
+const Blogpage = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Our news</h1>
+      <ul>
+        {posts.map((post) => (
+          <Link key={post.id} to={`/posts/${post.id}`}>
+            <li>{post.title}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export { Blogpage };
